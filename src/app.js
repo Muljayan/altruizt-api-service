a98272e9a5724e5b0bf7312dd1ff3a858c9b1447
Muljayan <37667437+Muljayan@users.noreply.github.com>
2021-07-19 00:23:26 +0530

    removed unnecessary helmet configs

--- src/app.js
+++ src/app.js
@@ -11,32 +11,6 @@ const app = express();
 
 // Express middlewares
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-  defaultSrc: [
-    '*',
-    'https://api.altruizt.xyz',
-    'https://altruizt.xyz',
-  ],
-  styleSrc: [
-    '\'self\'',
-    '\'unsafe-inline\'',
-    'https://*.googleapis.com',
-    'https://api.altruizt.xyz',
-    'https://altruizt.xyz',
-  ],
-  scriptSrc: [
-    '\'self\'',
-    '\'unsafe-inline\'',
-    'https://api.altruizt.xyz',
-    'https://altruizt.xyz',
-  ],
-  contentSrc: [
-    '\'self\'',
-    '\'unsafe-inline\'',
-    'https://api.altruizt.xyz',
-    'https://altruizt.xyz',
-  ],
-}));
 app.use(json({ limit: '10mb' }));
 app.use(cors());
 app.use(express.static(ASSETS_DIRECTORY));
